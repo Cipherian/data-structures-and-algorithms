ff3fb57478761602dc855e8815ca146e54c4c378
Daniel Brott <danbrott42@gmail.com>
2022-07-18 21:09:51 -0700

    passed tests 5 and 6

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -69,7 +69,11 @@ Write a function named forEachTwoToThe that produces the same output as your for
 ------------------------------------------------------------------------------------------------ */
 
 const forEachTwoToThe = (arr) => {
-  // Solution code here...
+  let twoPowerArray = [];
+  arr.forEach((element) => {
+    twoPowerArray.push(Math.pow(2, element));
+  });
+  return twoPowerArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -79,7 +83,10 @@ Write a function named mapTwoToThe that produces the same output as your forLoop
 ------------------------------------------------------------------------------------------------ */
 
 const mapTwoToThe = (arr) => {
-  // Solution code here...
+  let mapArr = arr.map((n) => {
+    return Math.pow(2,n);
+  });
+  return mapArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
