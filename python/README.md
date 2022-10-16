438e6e4824a46d2437b0dc84d45f1ce93c1a71a4
Daniel Brott <danbrott42@gmail.com>
2022-10-16 00:35:01 +0000

    fixed up readme

--- python/README.md
+++ python/README.md
@@ -1,9 +1,13 @@
 # Table Of Contents
 
-Code Challenges
+## Code Challenges
 
 - Code Challenge 1: [reverse_array](./code_challenges/code-challenge1/README.md)
 
 - Code Challenge 2: [insert_shift_list](./code_challenges/code-challenge2/README.md)
 
 - Code Challenge 3: [binary_search](./code_challenges/code-challenge3/README.md)
+
+## Data Structures and Algorithms
+
+- Linked lists: [Linked_lists](./data_structures/linked_list.py)
--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -1,16 +1,48 @@
-class LinkedList:
+class Node:
     """
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
 
-    def some_method(self):
-        # method body here
-        pass
+    def __str__(self):
+        return str(self.value)
 
+    def __repr__(self):
+        return f'<Node: Value = {self.value} | Next = {self.next}>'
+
+class LinkedList:
+
+    def __init__(self, head=None):
+        self.head = head
+
+    def __str__(self):
+        string = ""
+        current = self.head
+        while current is not None:
+            string += f'{{ {current.value} }} -> '
+            # string.strip(',')
+            current = current.next
+        string += 'NULL'
+        return string
+
+    def insert(self, value):
+        node_item = Node(value)
+
+        if self.head is not None:
+            node_item.next = self.head
+        self.head = node_item
+
+    def includes(self, value)  :
+        current = self.head
+
+        while current is not None:
+            if current.value == value:
+                return True
+            current = current.next
+        return False
 
 class TargetError:
     pass
