dc72a68303dad13a977d13930ffb5185a3c98be8
Daniel Brott <danbrott42@gmail.com>
2022-11-08 06:35:21 +0000

    updated code

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -1,5 +1,18 @@
 from data_structures.stack import Stack
 
 
-class PseudoQueue:
-    pass
+class PseudoQueue(Stack):
+
+    def enqueue(self, value):
+        temp = PseudoQueue()
+        while self.is_empty() is False:
+            temp.push(self.pop())
+        self.push(value)
+        while temp.is_empty() is False:
+            self.push(temp.pop())
+
+    def dequeue(self):
+        if self.is_empty() is False:
+            return self.pop()
+
+
