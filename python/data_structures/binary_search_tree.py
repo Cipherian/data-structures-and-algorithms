79e500817284b73c4379ac73837e191a6d4f97b9
Daniel Brott <danbrott42@gmail.com>
2022-11-20 01:34:54 +0000

    binary_tree passing tests

--- python/data_structures/binary_search_tree.py
+++ python/data_structures/binary_search_tree.py
@@ -1,4 +1,4 @@
-from data_structures.binary_tree import BinaryTree
+from data_structures.binary_tree import BinaryTree, Node
 
 
 class BinarySearchTree(BinaryTree):
@@ -6,10 +6,51 @@ class BinarySearchTree(BinaryTree):
     Put docstring here
     """
 
-    def __init__(self):
-        # initialization here
-        pass
+    def __init__(self, root=None):
+        self.root = root
 
-    def some_method(self):
-        # method body here
-        pass
+    def add(self, value):
+        node = Node(value)
+
+        if self.root is None:
+            self.root = node
+            return
+
+        def walk(root, node):
+            if node.value < root.value:
+                if root.left:
+                    walk(root.left, node)
+                else:
+                    root.left = node
+
+            else:
+                if root.right:
+                    walk(root.right, node)
+                else:
+                    root.right = node
+
+        walk(self.root, node)
+
+    def contains(self, value):
+        contains_list = []
+
+        def walk(root, node):
+            if root is None:
+                return
+
+            if root:
+                if root.value == value:
+                    contains_list.append(node)
+                else:
+                    if value < root.value:
+                        if root.left:
+                            walk(root.left, node)
+                    else:
+                        if root.right:
+                            walk(root.right, node)
+
+        walk(self.root, None)
+
+        return len(contains_list) == 1
+
+
