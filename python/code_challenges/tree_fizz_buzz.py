55b37bb541fffdc04b9a1dac7df5c8022d433536
Daniel Brott <danbrott42@gmail.com>
2022-12-02 05:17:28 +0000

    passing tests

--- python/code_challenges/tree_fizz_buzz.py
+++ python/code_challenges/tree_fizz_buzz.py
@@ -1,5 +1,34 @@
 from data_structures.binary_tree import BinaryTree
+from data_structures.queue import Queue
+from data_structures.kary_tree import KaryTree, Node
+
+def fizz_buzz_function(num):
+    if num % 15 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return str(num)
+
+def fizz_buzz_tree(tree):
+    k_tree = KaryTree()
+    k_tree.root = Node()
+    queue = Queue()
+
+    queue.enqueue((tree.root, k_tree.root))
+    while not queue.is_empty():
+        old_node, new_node = queue.dequeue()
+        new_node.value = fizz_buzz_function(old_node.value)
+
+        for child in old_node.children:
+            new_child = Node()
+            new_node.children.append(new_child)
+            queue.enqueue((child, new_child))
+
+    return k_tree
 
 
-def fizz_buzz_tree():
-    pass
+
+
