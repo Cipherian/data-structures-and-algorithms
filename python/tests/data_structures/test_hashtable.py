a27aea8d19034fb338f17b03ba57f56d099982e5
Daniel Brott <danbrott42@gmail.com>
2023-01-15 19:27:08 +0000

    code added, tests added and tests passing

--- python/tests/data_structures/test_hashtable.py
+++ python/tests/data_structures/test_hashtable.py
@@ -6,7 +6,7 @@ def test_exists():
     assert Hashtable
 
 
-@pytest.mark.skip("TODO")
+
 def test_get_apple():
     hashtable = Hashtable()
     hashtable.set("apple", "Used for apple sauce")
@@ -15,7 +15,7 @@ def test_get_apple():
     assert actual == expected
 
 
-@pytest.mark.skip("TODO")
+
 def test_internals():
     hashtable = Hashtable(1024)
     hashtable.set("ahmad", 30)
@@ -24,11 +24,31 @@ def test_internals():
 
     actual = []
 
-    # NOTE: purposely breaking encapsulation to test the "internals" of Hashmap
     for item in hashtable._buckets:
         if item:
-            actual.append(item.display())
+            actual.append(item)
 
-    expected = [[["silent", True], ["listen", "to me"]], [["ahmad", 30]]]
+    expected = [["silent", True], ["listen", "to me"], ["ahmad", 30]]
 
     assert actual == expected
+
+
+def test_hash_function_type():
+    hashtable = Hashtable(1024)
+    key = "test_key"
+    assert isinstance(hashtable.hash_function(key), int)
+
+
+def test_hash_function_size():
+    hashtable = Hashtable(1024)
+    key = "test_key"
+    assert hashtable.hash_function(key) <= hashtable.size
+
+
+def test_hash_delete_function():
+    hashtable = Hashtable(1024)
+    key = "test_key"
+    value = "test_value"
+    hashtable.set(key, value)
+    hashtable.delete(key)
+    assert hashtable._buckets[hashtable.hash_function(key)] is None
