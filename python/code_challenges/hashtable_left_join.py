82185b4b99829333d85c578fa0f69dcfb7d6bf49
Daniel Brott <danbrott42@gmail.com>
2023-01-24 05:21:44 +0000

    message

--- python/code_challenges/hashtable_left_join.py
+++ python/code_challenges/hashtable_left_join.py
@@ -1,5 +1,12 @@
-from data_structures.hashtable import Hashtable
 
 
-def left_join():
-    pass
+
+def left_join(map1, map2):
+    result = []
+    for key in map1.keys():
+        map1_value = map1[key]
+        map2_value = map2.get(key, None)
+        result.append([key, map1_value, map2_value])
+    return result
+
+
